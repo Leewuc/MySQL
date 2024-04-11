87ec0f681dc5c34eef4c16cf7b31b312f670ce8f
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-04-11 17:02:49 +0900

    Add files via upload

--- university/prac1.sql
+++ university/prac1.sql
@@ -3,6 +3,11 @@
 -- FROM instructor
 -- WHERE dept_id = 2;
 
+-- SELECT name
+-- FROM instructor INNER JOIN department on instructor.dept_id = department.ID
+-- WHERE department.dept_name = "Comp. Sci.";
+
+# Q2. 2017년 1학기에 열린 과목을 담당한 모든 강사의 이름을 출력하세요.
 SELECT name
-FROM instructor INNER JOIN department on instructor.dept_id = department.ID
-WHERE department.dept_name = "Comp. Sci.";
+FROM instructor INNER JOIN inst_dept on instructor.ID = inst_dept.i_id
+WHERE inst_dept.semester = "2017-1";
